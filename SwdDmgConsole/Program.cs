08db4200bd53bf05859bcb6481d966d858fc1c9f
Ivor Lewis <illiated@gmail.com>
2023-12-12 16:02:08 +0000

    Add project files.

---
+++ SwdDmgConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using SwdDmgConsole;
+
+Random random = new Random();
+
+SwdDmg swdDmg = new SwdDmg(RollDice());
+
+while (true)
+{
+
+}
+
+int RollDice()
+{
+    return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+}
---
+++ SwdDmgConsole/SwdDmg.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwdDmgConsole
+{
+    class SwdDmg
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        public SwdDmg(int initialRoll)
+        {
+            roll = initialRoll;
+            CalculateDamage();
+        }
+
+        /// <summary>
+        /// Contains calculated damage.
+        /// </summary>
+        public int Damage { get; private set; }
+
+        private int roll;
+
+        /// <summary>
+        /// Sets the roll and also calculates the damage.
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+
+        /// <summary>
+        /// Sets the flamimg condition and updates the damage calculation.
+        /// True if the sword id flaming.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool magic;
+
+        /// <summary>
+        /// Sets the magic condition and updates the damage calculation.
+        /// True of the sword is magic.
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1;
+            if (Magic)
+            {
+                magicMultiplier = 1.75M;
+            }
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+
+
+        }
+
+    }
+}
+
