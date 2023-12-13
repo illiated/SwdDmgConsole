616b38781fab902b3a6afbde0a196c1c4cc723b4
Ivor Lewis <illiated@gmail.com>
2023-12-13 16:36:02 +0000

    Minor console input adjustment.

--- SwdDmgConsole/Program.cs
+++ SwdDmgConsole/Program.cs
@@ -6,7 +6,7 @@ SwdDmg swdDmg = new SwdDmg(RollDice());
 
 while (true)
 {
-    Console.WriteLine("0 for normal, 1 for magic, 2 for flaming, 3 for magic/flaming, or any other key to quit.");
+    Console.WriteLine("Type '0' for normal, '1' for magic, '2' for flaming, '3' for magic/flaming, or any other key to quit.");
     char inputKey = Console.ReadKey(true).KeyChar;
     if (inputKey != '0' && inputKey != '1' && inputKey != '2' && inputKey != '3') return;
     swdDmg.Roll = RollDice();
