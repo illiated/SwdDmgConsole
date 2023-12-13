98dcb04646d0e0e70025d890ee31c875a1020171
Ivor Lewis <illiated@gmail.com>
2023-12-13 13:19:22 +0000

    Update Program Class with comments also.

--- SwdDmgConsole/Program.cs
+++ SwdDmgConsole/Program.cs
@@ -6,6 +6,10 @@ SwdDmg swdDmg = new SwdDmg(RollDice());
 
 while (true)
 {
+    Console.WriteLine("0 for normal, 1 for magic, 2 for flaming, 3 for magic/flaming, or any other key to quit.");
+    char inputKey = Console.ReadKey(true).KeyChar;
+    if (inputKey != '0' && inputKey != '1' && inputKey != '2' && inputKey != '3') return;
+    //whats next for the app to do? type this here!
 
 }
 
